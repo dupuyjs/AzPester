e7abd278eadd069aeae4184761d414586e2e94e6
Jean-Sébastien Dupuy <jdupuy@microsoft.com>
2021-06-01 16:55:27 +0200

    Update keyvault resource to support networkAcls and expressions

--- Source/Resources/Security/KeyVault/KeyVault.Tests.ps1
+++ Source/Resources/Security/KeyVault/KeyVault.Tests.ps1
@@ -19,7 +19,7 @@ Describe 'Key Vault <name> Acceptance Tests' -Tag 'Security' -ForEach $KeyVaults
         $keyVault = Get-KeyVault -Definition $Definition -Name $name -Context $context
     }
 
-    Context 'Key Vault <name>'{
+    Context 'Key Vault <name>' {
         It 'Validate key vault <name> has been provisioned' {
             $keyVault | Should -Not -BeNullOrEmpty
         }
@@ -30,45 +30,67 @@ Describe 'Key Vault <name> Acceptance Tests' -Tag 'Security' -ForEach $KeyVaults
         }
     }
 
-    Context 'Access Policies for <identity.name>' -ForEach $accessPolicies {
+    Context 'Network Rule Set' -ForEach $accessPolicies {
         BeforeAll {
             [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
-            $objectId = Get-IdentityObjectId -Definition $Definition -Type $identity.type -Name $identity.name -Context $identity.context
-            [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
-            $policy = $keyVault.AccessPolicies | Where-Object {$_.ObjectId -eq $objectId}
+            $networkAcls = $keyVault.NetworkAcls
         }
 
-        It 'Validate access policies for <identity.name> have been provisioned'{
+        It 'Validate network rule <propertyName> is <propertyValue>' -ForEach $networkAclsProperties {
+            $propertyName | Should -Not -BeNullOrEmpty
+            $propertyValue | Should -Not -BeNullOrEmpty
+            $networkAcls.$propertyName | Should -Be $propertyValue
+        }
+    }
+
+    Context 'Access Policies for <displayValue>' -ForEach $accessPolicies {
+        BeforeAll {
+            if (!$objectId) {
+                [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+                $objectId = Get-IdentityObjectId -Definition $Definition -Type $identity.type -Name $identity.name -Context $identity.context
+            }
+            [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+            $policy = $keyVault.AccessPolicies | Where-Object { $_.ObjectId -eq $objectId }
+        }
+
+        It 'Validate access policies for <displayValue> have been provisioned' {
             $objectId | Should -Not -BeNullOrEmpty
             $policy | Should -Not -BeNullOrEmpty
         }
         It 'Validate access policies <propertyName> is <propertyValue>' -ForEach $properties {
             $propertyName | Should -Not -BeNullOrEmpty
             $propertyValue | Should -Not -BeNullOrEmpty
-            ($policy.$propertyName | Sort-Object) | Should -Be ($propertyValue| Sort-Object)
+
+            if($propertyValue.GetType().Name -eq "String") {
+                $propertyValue = $propertyValue.Split()
+            }
+            
+            ($policy.$propertyName | Sort-Object) | Should -Be ($propertyValue | Sort-Object)
         }
     }
 
-    Context 'Key <name>' -ForEach $keys {
-        BeforeAll {
-            [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
-            $key = Get-KeyVaultKey -Definition $Definition -VaultName $keyVault.VaultName -Name $name -Context $context
-        }
-
-        It 'Validate key is Enabled' {
+    Context 'Keys' {
+        It 'Validate key <name> is Enabled' -ForEach $keys {
+            $key = Get-KeyVaultKey `
+                -Definition $Definition `
+                -VaultName $keyVault.VaultName `
+                -Name $name `
+                -Context $context
+            
             $key | Should -Not -BeNullOrEmpty
             $key.Name | Should -Be $name
             $key.Enabled | Should -BeTrue
         }
     }
 
-    Context 'Secret <name>' -ForEach $secrets {
-        BeforeAll {
-            [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
-            $secret = Get-KeyVaultSecret -Definition $Definition -VaultName $keyVault.VaultName -Name $name -Context $context
-        }
+    Context 'Secrets' {
+        It 'Validate secret <name> is Enabled' -ForEach $secrets {
+            $secret = Get-KeyVaultSecret `
+                -Definition $Definition `
+                -VaultName $keyVault.VaultName `
+                -Name $name `
+                -Context $context
 
-        It 'Validate secret is Enabled'  {
             $secret | Should -Not -BeNullOrEmpty
             $secret.Name | Should -Be $name
             $secret.Enabled | Should -BeTrue
@@ -81,13 +103,13 @@ Describe 'Key Vault <name> Acceptance Tests' -Tag 'Security' -ForEach $KeyVaults
             $certificate = Get-KeyVaultCertificate -Definition $Definition -VaultName $keyVault.VaultName -Name $name -Context $context
         }
 
-        It 'Validate certificate is Enabled'  {
+        It 'Validate certificate is Enabled' {
             $certificate | Should -Not -BeNullOrEmpty
             $certificate.Name | Should -Be $name
             $certificate.Enabled | Should -BeTrue
         }
         It 'Validate certificate <propertyName> is <displayValue>' -ForEach $properties {
-            if($propertyName -eq 'expiration') {
+            if ($propertyName -eq 'expiration') {
                 $certificate.Expires | Should -BeGreaterThan (Get-Date).AddDays($propertyValue)
             }
             else {
